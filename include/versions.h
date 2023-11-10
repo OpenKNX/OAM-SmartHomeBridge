258688a9d05b46142f1ca832563661b3686eed44
Michael Geramb <mail@michael-geramb.at>
2023-11-10 17:51:53 +0100

    Fix ETS app truncated text

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "6a76bcf"
+#define MAIN_Version "ed38334"
 #define KNX_Version "f8f34c5"
 #define MODULE_Common_Version "1.0.0+413621e"
 #define MODULE_Common_ETS 16
