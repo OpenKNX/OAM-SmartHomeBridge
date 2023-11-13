b3feaa80250490e6e0d5024217fc98999d21c641
Michael Geramb <mail@michael-geramb.at>
2023-11-13 19:30:37 +0100

    Revert "Switch back to 50 channels, seems to be the maximum stable number"
    This reverts commit e2db953357b7266cdba7ee24c77ed13d9722431d.

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "b7cd4ea"
+#define MAIN_Version "258688a"
 #define KNX_Version "f8f34c5"
 #define MODULE_Common_Version "1.0.0+413621e"
 #define MODULE_Common_ETS 16
