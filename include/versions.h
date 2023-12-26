91d7d15db1bb86266f0aea85a18e8e9e7fdea3c2
Michael Geramb <mail@michael-geramb.at>
2023-12-26 09:12:57 +0100

    First version with generated XML, not yet working

--- include/versions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define MAIN_Version "b89b02c"
-#define KNX_Version "361620f"
-#define MODULE_Common_Version "1.0.9+6e8d114"
-#define MODULE_Common_ETS 16
-#define MODULE_LogicModule_Version "2.0.3+ddc9cfc"
-#define MODULE_LogicModule_ETS 32
---
+++ src/Baggages/Help_de/BRI-Doc.md
@@ -0,0 +1,11 @@
+### Header 1
+
+Help text 1
+
+### Header 2
+
+Help text 2
+
+### Header 3
+
+Help text 3
