056ab3a8c0901cd0a32bf299c547976bca3da434
Michael Geramb <lasso_riefen0m@icloud.com>
2025-08-03 08:45:47 +0200

    Fix cooling mode for thermostat

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ OpenKNX::Led::GPIO *led3 = nullptr;
 void setup()
 {
 
-  const uint8_t firmwareRevision = 2;
+  const uint8_t firmwareRevision = 3;
 #ifdef PROG_LED_PIN2
   led2 = new OpenKNX::Led::GPIO();
   led2->init(PROG_LED_PIN2, PROG_LED_PIN2_ACTIVE_ON);
